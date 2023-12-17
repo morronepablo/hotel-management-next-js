95820347cc3e449496d91600eb6d406335cf3f4c
morronepablo <morronepablo@gmail.com>
2023-12-17 01:06:58 -0300

    subiendo proyecto hotel-management-next-js

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'hotel-management',
+
+  projectId: '21ryniqd',
+  dataset: 'production',
+
+  basePath: "/studio",
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
